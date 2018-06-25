6995a6652aa63e52f174c2d4d5d0acf64ce29099
tatoMa <Jason.ma1904@gmail.com>
2018-06-25 19:34:42 +1000

    modified source codes

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,22 @@
 </template>
 
 <script>
-import Stock from './components/Stock.vue'
+  import Stock from './components/Stock.vue'
 
-export default {
-  name: 'app',
-  components: {
-      Stock
+  export default {
+    name: 'app',
+    components: {
+        Stock
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!--<style>-->
+<!--#app {-->
+  <!--font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+  <!-- -webkit-font-smoothing: antialiased;-->
+  <!-- -moz-osx-font-smoothing: grayscale;-->
+  <!--text-align: center;-->
+  <!--color: #2c3e50;-->
+<!--}-->
+<!--</style>-->
--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,75 +1,133 @@
 <template>
-  <div id="container" style="width:100% height:400px">
-  </div>
+    <v-app>
+        <v-toolbar dark color="primary">
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title class="white--text">Stock Analysis</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>search</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>apps</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>refresh</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>more_vert</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-layout>
+            <v-flex xs12 sm10 offset-sm1>
+                <v-card>
+                    <v-card-media id="container" style="width:100%; height:400px">
+                    </v-card-media>
+                    <v-card-title primary-title>
+                        <v-flex>
+                            <v-layout row wrap>
+                                <v-flex xs12 sm4>
+                                    <v-subheader v-text="'Select Identifier'"></v-subheader>
+                                </v-flex>
+                                <v-flex xs12 sm8>
+                                    <v-select
+                                            :items="identifiers"
+                                            v-model="identifierSelected"
+                                            label="Select"
+                                            autocomplete
+                                    ></v-select>
+                                </v-flex>
+                            </v-layout>
+                            <div>
+                                <h2 class="text-xs-center">find the maximum profit</h2>
+                            </div>
+                        </v-flex>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="orange">Share</v-btn>
+                        <v-btn flat color="orange">Explore</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        {{identifierSelected}}
+    </v-app>
 </template>
 
 <script>
-// import Highcharts from 'highcharts'
-import $ from "jquery"
 import Highcharts from 'highcharts/highstock'
 var https = require("https")
-// import Intrinio from './intrinio'
 
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'StockChart',
+    // components: {
+    // },
+    data () {
+        return {
+            username : "9abe0f925790d738fa8b5309b848b921",
+            password : "5353dd541c69225b311ac17fd6a53bb1",
+            identifierSelected: 'QCOM',
+            identifiers: ['QCOM','AAPL']
+        }
+    },
+    methods: {
+        // searchAppointment (text) {
+        //     this.searchText = text
+        // }
+    },
+    mounted () {
+        // authorization of intrinio data
+        var self = this
+        var auth = "Basic " + new Buffer(this.username + ':' + this.password).toString('base64')
+
+        var company =""
+        var dataForChart =[]
+
+        var request = https.request({
+            method: "GET",
+            host: "api.intrinio.com",
+            path: `/historical_data?identifier=${self.identifierSelected}&item=close_price&start_date=2017-06-01&end_date=2018-06-01&sort_order=asc&page_size=999`,
+            headers: {
+                "Authorization": auth
+            }
+        }, (response) => {
+            var json = ""
+            response.on('data', function (chunk) {
+                json += chunk
+            })
+            response.on('end', function() {
+                company = JSON.parse(json)
+                // console.log(company)
+
+                // format data from history intrinio.com to the format using in HighCharts
+                company.data.forEach(function (str) {
+                    var singleData = []
+                    singleData.push(Date.parse(str.date))
+                    singleData.push(str.value)
+                    dataForChart.push(singleData)
+                })
+
+                // Create the chart
+                Highcharts.stockChart('container', {
+                    rangeSelector: {
+                        selected: 1
+                    },
+                    title: {
+                        text: 'QUALCOMM'
+                    },
+                    series: [{
+                        name: 'QCOM',
+                        data: dataForChart,
+                        tooltip: {
+                            valueDecimals: 2
+                        }
+                    }]
+                })
+            })
+        })
+        request.end()
+    },
 }
 
-// authorization of intrinio data
-var username = "9abe0f925790d738fa8b5309b848b921"
-var password = "5353dd541c69225b311ac17fd6a53bb1"
-var auth = "Basic " + new Buffer(username + ':' + password).toString('base64')
-var company =""
-var dataForChart =[]
-
-var request = https.request({
-    method: "GET",
-    host: "api.intrinio.com",
-    path: "/historical_data?identifier=QCOM&item=close_price&start_date=2017-06-01&end_date=2018-06-01&sort_order=asc&page_size=999",
-    headers: {
-        "Authorization": auth
-    }
-}, function(response) {
-    var json = ""
-    response.on('data', function (chunk) {
-        json += chunk
-    })
-    response.on('end', function() {
-        company = JSON.parse(json)
-        console.log(company)
-        company.data.forEach(function (str) {
-            // console.log(str)
-            var singleData = []
-            singleData.push(Date.parse(str.date))
-            singleData.push(str.value)
-            dataForChart.push(singleData)
-        })
-        console.log(dataForChart)
-        // Create the chart
-        Highcharts.stockChart('container', {
-
-            rangeSelector: {
-                selected: 1
-            },
-
-            title: {
-                text: 'QUALCOMM'
-            },
-
-            series: [{
-                name: 'QCOM',
-                data: dataForChart,
-                tooltip: {
-                    valueDecimals: 2
-                }
-            }]
-        })
-    })
-})
-
-request.end()
 
 </script>
 
