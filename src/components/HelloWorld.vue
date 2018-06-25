123a7412060bd438b1998f4b282391525f938a88
tatoMa <Jason.ma1904@gmail.com>
2018-06-25 13:59:22 +1000

    successful implement the first high chart stock line

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script>
-import Highcharts from 'highcharts'
+// import Highcharts from 'highcharts'
 import $ from "jquery"
+import Highcharts from 'highcharts/highstock'
 
 export default {
   name: 'HelloWorld',
@@ -14,55 +15,59 @@ export default {
     msg: String
   }
 }
-$(function () {
-    let myChart = Highcharts.chart('container', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
-});
-// Highcharts.chart('container', { /*Highcharts options*/ });
 
-// $.getJSON('https://www.highcharts.com/samples/data/aapl-c.json', function (data) {
-//     // Create the chart
-//     Highcharts.stockChart('container', {
-//
-//
-//         rangeSelector: {
-//             selected: 1
+// sample highcharts testing
+// $(function () {
+//     let myChart = Highcharts.chart('container', {
+//         chart: {
+//             type: 'bar'
 //         },
-//
 //         title: {
-//             text: 'AAPL Stock Price'
+//             text: 'Fruit Consumption'
 //         },
-//
-//         series: [{
-//             name: 'AAPL',
-//             data: data,
-//             tooltip: {
-//                 valueDecimals: 2
+//         xAxis: {
+//             categories: ['Apples', 'Bananas', 'Oranges']
+//         },
+//         yAxis: {
+//             title: {
+//                 text: 'Fruit eaten'
 //             }
+//         },
+//         series: [{
+//             name: 'Jane',
+//             data: [1, 0, 4]
+//         }, {
+//             name: 'John',
+//             data: [5, 7, 3]
 //         }]
 //     });
 // });
+// Highcharts.chart('container', { /*Highcharts options*/ });
+
+$.getJSON('https://www.highcharts.com/samples/data/aapl-c.json', function (data) {
+    // Create the chart
+    Highcharts.stockChart('container', {
+
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'AAPL Stock Price'
+        },
+
+        series: [{
+            name: 'AAPL',
+            data: data,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
+});
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
